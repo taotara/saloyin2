e60db9eff52853b2f6604729a601de0b54812d49
taotara <taotara@yahoo.com>
2023-12-13 14:34:38 +0100

    Hero area improved

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeroComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
